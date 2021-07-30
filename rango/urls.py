c83da790f6335bef8cda4c9220581f75c5fab837
Sofia Christou <2605675c@student.gla.ac.uk>
2021-07-30 11:41:16 +0100

    Chapter 9 tutorial 9.8)

--- rango/urls.py
+++ rango/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/', views.show_category,
         name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
-        path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
 ]
 
