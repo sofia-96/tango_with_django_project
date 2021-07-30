c4448ab8806d9301e9835675521b67535df79d1b
Sofia Christou <2605675c@student.gla.ac.uk>
2021-07-30 10:40:05 +0100

    Chapter 9 tutorial 9.6

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     NAME_MAX_LENGTH = 50
@@ -13,8 +14,9 @@ class Category(models.Model):
         super(Category, self).save(*args, **kwargs)
 class Meta:
     verbose_name_plural = 'Categories'
-def __str__(self):
-    return self.name
+
+    def __str__(self):
+        return self.name
 class Page(models.Model):
     TITLE_MAX_LENGTH = 130
     URL_MAX_LENGTH = 220
@@ -22,5 +24,14 @@ class Page(models.Model):
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
-def __str__(self):
-    return self.titl
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
